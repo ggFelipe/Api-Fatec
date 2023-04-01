29924f93daba0cb8eb05f40fa54ed555488d9564
Christian Felipe Oller de Oliveira <christianfelipe_09@hotmail.com>
2023-03-31 22:09:27 -0300

    first commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,33 @@
+import { where } from "sequelize";
+import User from "../models/userModel.js";
+
+async function getAllUsers(req,res){
+    const user = await User.findAll()
+    res.json(user)
+}
+
+async function createUser(req,res){
+    const user = await User.create(req.body)
+    res.json(user)
+}
+
+async function deleteUser(req,res){
+    const user = await User.destroy({where: {id: req.params.id}})
+    res.json(user)
+}
+
+async function updateUser(req,res){
+    const user = await User.update(req.body,{
+        where:{
+            id: req.params.id
+        }
+    })
+    res.json(user)
+}
+
+async function getUserById(req,res){
+    const user = await User.findByPk(req.params.id)
+    res.json(user)
+}
+
+export default {getAllUsers, createUser, deleteUser, updateUser, getUserById}
---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize"
+import db from "../db.js"
+
+const User = db.define("user",{
+
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+
+})
+
+export default User
